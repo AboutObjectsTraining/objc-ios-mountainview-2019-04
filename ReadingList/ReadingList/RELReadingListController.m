d8757cea067ee905df1c915ec8cad822a03e582a
Jonathan Lehr <jonathan@aboutobjects.com>
2019-05-03 16:51:25 -0700

    End of Day 5

---
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -0,0 +1,17 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "RELReadingListController.h"
+
+@implementation RELReadingListController
+
+- (IBAction)done:(UIStoryboardSegue *)unwindSegue {
+    // TODO: sync UI and save changes.
+}
+
+- (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
+    // Empty method.
+}
+
+
+@end
